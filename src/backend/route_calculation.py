f51306fb272bddbb58c56c75174fd3805135f88d
Christian Beck <beck.christian03@gmail.com>
2026-01-11 23:05:47 +0100

    refactor: missing commas

--- src/backend/route_calculation.py
+++ src/backend/route_calculation.py
@@ -17,7 +17,7 @@ def connect_db():
     return mysql.connector.connect(**DB)
 
 def get_latest_bus_gps(cur, bus_id):
-    cur.execute("select * from BusPosition where fk_bus_id=%s order by gps_send_at desc", (bus_id))
+    cur.execute("select * from BusPosition where fk_bus_id=%s order by gps_send_at desc", (bus_id,))
     row = cur.fetchone()
     return row if row else None
 # get person gps
@@ -25,14 +25,14 @@ def get_latest_bus_gps(cur, bus_id):
 def get_bus_id_from_line(cur,):
     cur.execute("select id from BusLine where line_name=1407")
     line = cur.fetchone()
-    cur.execute("select * from Bus where fk_line_id=%s", (line[0]))
+    cur.execute("select * from Bus where fk_line_id=%s", (line[0],))
     bus = cur.fetchone()
     return bus if bus else None
 
 def get_next_stops(cur, next_stop_id,):
-    cur.execute("select sequenc_order from LineStops where fk_stop_id=%s", (next_stop_id))
+    cur.execute("select sequenc_order from LineStops where fk_stop_id=%s", (next_stop_id,))
     next_stop_order = cur.fetchone()
-    cur.execute("select * from LineStops where sequenc_order>=%s", (next_stop_order[0]))
+    cur.execute("select * from LineStops where sequenc_order>=%s", (next_stop_order[0],))
     next_stops = cur.fetchall()
     return next_stops if next_stops else None
 # missing main prob?
