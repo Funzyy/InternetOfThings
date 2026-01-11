5342dab844d81265cb7512a3ed1f0378cad78ed0
Christian Beck <beck.christian03@gmail.com>
2026-01-11 18:02:11 +0100

    feat: database stuff

--- src/backend/route_calculation.py
+++ src/backend/route_calculation.py
@@ -1,4 +1,20 @@
-# databse stuff - copy from maigo
+import time
+import requests
+import mysql.connector
+from mysql.connector import Error
+
+from db_init import init_db  # ggf. Import-Pfad anpassen
+
+DB = {
+    "host": "127.0.0.1",
+    "user": "root",
+    "password": "maigoroot",
+    "database": "BusTracker",
+    "port": 3306,
+}
+
+def connect_db():
+    return mysql.connector.connect(**DB)
 # get bus gps
 # get person gps
 # get line info
