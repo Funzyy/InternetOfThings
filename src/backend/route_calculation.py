5419bb3f174d2bc601acd103ba934bd3e357969a
Christian Beck <beck.christian03@gmail.com>
2026-01-11 23:19:19 +0100

    refactor: code cleanup

--- src/backend/route_calculation.py
+++ src/backend/route_calculation.py
@@ -18,21 +18,18 @@ def connect_db():
 
 def get_latest_bus_gps(cur, bus_id):
     cur.execute("select * from BusPosition where fk_bus_id=%s order by gps_send_at desc", (bus_id,))
-    row = cur.fetchone()
-    return row if row else None
+    return cur.fetchone()
 # get person gps
 
 def get_bus_id_from_line(cur,):
     cur.execute("select id from BusLine where line_name=1407")
     line = cur.fetchone()
     cur.execute("select * from Bus where fk_line_id=%s", (line[0],))
-    bus = cur.fetchone()
-    return bus if bus else None
+    return cur.fetchone()
 
 def get_next_stops(cur, next_stop_id,):
     cur.execute("select sequenc_order from LineStops where fk_stop_id=%s", (next_stop_id,))
     next_stop_order = cur.fetchone()
-    cur.execute("select * from LineStops where sequenc_order>=%s", (next_stop_order[0],))
-    next_stops = cur.fetchall()
-    return next_stops if next_stops else None
+    cur.execute("select * from LineStops where sequenc_order>=%s order by sequenc_order", (next_stop_order[0],))
+    return cur.fetchall()
 # missing main prob?
