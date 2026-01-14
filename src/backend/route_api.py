d2f576b20195c59b8b1905b00e9bea98fbf96d82
Christian Beck <beck.christian.ser@gmail.com>
2026-01-14 09:23:31 +0100

    refactor: added api key and urls

--- src/backend/route_api.py
+++ src/backend/route_api.py
@@ -10,3 +10,7 @@ import requests
 ## destiantions können gleich bleiben
 ## probe request für tests und evtl. andere gewichtung/ergebnisse von ors
 print(route_calculation.get_api_gps_data(2, 2))
+
+ors_api_key = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjVkYTMzMzUzNGQ5ZTQyNWQ5YjY3MzYxYjJiN2IxMzRjIiwiaCI6Im11cm11cjY0In0="
+ors_api_url_bus = "https://api.openrouteservice.org/v2/matrix/driving-hgv"
+ors_api_url_car = "https://api.openrouteservice.org/v2/matrix/foot-walking"
