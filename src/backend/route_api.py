da9f32a051fbd30651a357753589af4450ce434c
Christian Beck <beck.christian03@gmail.com>
2026-01-18 22:07:29 +0100

    refactor: max stops variable

--- src/backend/route_api.py
+++ src/backend/route_api.py
@@ -14,6 +14,7 @@ print(route_calculation.get_api_gps_data(2, 2))
 ors_api_key = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjVkYTMzMzUzNGQ5ZTQyNWQ5YjY3MzYxYjJiN2IxMzRjIiwiaCI6Im11cm11cjY0In0="
 ors_api_url_bus = "https://api.openrouteservice.org/v2/matrix/driving-hgv"
 ors_api_url_person = "https://api.openrouteservice.org/v2/matrix/foot-walking"
+max_stops = 5
 
 def builld_header():
     headers = {
