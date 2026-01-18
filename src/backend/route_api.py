1f645068b0878b9254eb2a6325459355cf5da2e2
Christian Beck <beck.christian03@gmail.com>
2026-01-18 20:51:54 +0100

    refactor: function to build api-head

--- src/backend/route_api.py
+++ src/backend/route_api.py
@@ -14,3 +14,10 @@ print(route_calculation.get_api_gps_data(2, 2))
 ors_api_key = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjVkYTMzMzUzNGQ5ZTQyNWQ5YjY3MzYxYjJiN2IxMzRjIiwiaCI6Im11cm11cjY0In0="
 ors_api_url_bus = "https://api.openrouteservice.org/v2/matrix/driving-hgv"
 ors_api_url_person = "https://api.openrouteservice.org/v2/matrix/foot-walking"
+
+def builld_header():
+    headers = {
+        "Accept": "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8",
+        "Authorization": ors_api_key,
+        "Content-Type": "application/json; charset=utf-8"
+    }
