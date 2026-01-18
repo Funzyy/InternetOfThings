75c90e58c227697f9778294d7c79e84867189655
Christian Beck <beck.christian03@gmail.com>
2026-01-19 00:09:14 +0100

    refactor: working api baseline

--- src/backend/route_api.py
+++ src/backend/route_api.py
@@ -1,3 +1,5 @@
+from email.quoprimime import body_check
+
 import route_calculation
 import requests
 
@@ -9,7 +11,7 @@ import requests
 ## erst lon dann lat
 ## destiantions können gleich bleiben
 ## probe request für tests und evtl. andere gewichtung/ergebnisse von ors
-print(route_calculation.get_api_gps_data(2, 2))
+#print(route_calculation.get_api_gps_data(2, 2))
 
 ors_api_key = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjVkYTMzMzUzNGQ5ZTQyNWQ5YjY3MzYxYjJiN2IxMzRjIiwiaCI6Im11cm11cjY0In0="
 ors_api_url_bus = "https://api.openrouteservice.org/v2/matrix/driving-hgv"
@@ -17,11 +19,12 @@ ors_api_url_person = "https://api.openrouteservice.org/v2/matrix/foot-walking"
 max_stops = 5
 
 def build_header():
-    headers = {
+    header = {
         "Accept": "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8",
         "Authorization": ors_api_key,
         "Content-Type": "application/json; charset=utf-8"
     }
+    return header
 
 def build_locations(start_location, stop_locations):
     locations = [[start_location["lon"], start_location["lat"]]]
@@ -29,10 +32,10 @@ def build_locations(start_location, stop_locations):
         locations.append([stop["lon"], stop["lat"]])
     return locations
 
-def api_call(url, header, body):
+def api_call(url, header, specific_body):
     response = requests.post(
         url,
-        json=body,
+        json=specific_body,
         headers=header)
     print("Dumping plain text response:", response.text)
     return {
@@ -40,5 +43,45 @@ def api_call(url, header, body):
         "data": response.json(),
     }
 
+def get_possible_routes():
+    header = build_header()
+    if not header:
+        return print("header null"), None
 
+    gps_data = route_calculation.get_api_gps_data(1, 1)
+    if not gps_data:
+        return print("GPS data null"), None
+
+    next_stops = gps_data["next_stops"][:max_stops]
+
+    person_locations = build_locations(gps_data["person"], next_stops)
+    bus_locations = build_locations(gps_data["bus_gps"], next_stops)
+
+    # api call person
+    person_body = {
+        "locations": person_locations,
+        "sources": [0],
+        "destinations": list(range(1, len(person_locations))),
+        "id": "person_routes",
+        "metrics": ["duration"],
+    }
+    person_routes = api_call(ors_api_url_person, header, person_body)
+    print("person routes: ", person_routes)
+    # api call bus
+    bus_body = {
+        "locations": bus_locations,
+        "sources": [0],
+        "destinations": list(range(1, len(bus_locations))),
+        "id": "person_routes",
+        "metrics": ["duration"],
+    }
+    bus_routes = api_call(ors_api_url_bus, header, bus_body)
+    print("bus routes: ", bus_routes)
+    # routen vergleichen und schauen wo person schneller ist als bus
+
+    return print("nothing for now"), None
+
+# poly line von möglichen routen holen
+# polyline zurück geben
+get_possible_routes()
 
